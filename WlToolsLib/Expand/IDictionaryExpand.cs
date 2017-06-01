7e9d8fb82b66d198df06f93c3ae172aeabc5f313
wlfsky <raddleoj@sina.com>
2017-06-02 00:04:15 +0800

    字典 检查是否有值

---
+++ WlToolsLib/Expand/IDictionaryExpand.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace WlToolsLib.Expand
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class IDictionaryExpand
+    {
+        /// <summary>
+        /// 检查字典中是否有值
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static bool HasItem<TKey, TValue>(this IDictionary<TKey, TValue> self)
+        {
+            if (self.IsNotNull() && self.Any())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
