c53fbc9db80119230a3da4517dbe6831622fa061
wlfsky <raddleoj@sina.com>
2017-06-24 23:13:26 +0800

    CryptoExpand

---
+++ WlToolsLib/CryptoHelper/CryptoExpand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WlToolsLib.CryptoHelper
+{
+    public static class CryptoExpand
+    {
+        #region --加密解密扩展--
+        public static string AESCBCEncryption(this string self, string key)
+        {
+            ICryptoHelper c = new AESCBCCrypto("AESCBC").SetData(key);
+            return c.Encryption(self);
+        }
+
+        public static string AESCBCDecryption(this string self, string key)
+        {
+            ICryptoHelper c = new AESCBCCrypto("AESCBC").SetData(key);
+            return c.Decryption(self);
+        }
+        #endregion
+    }
+}
