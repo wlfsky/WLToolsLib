0088c4d406079465be20edbca092f698cf400dfd
wlfsky <raddleoj@sina.com>
2017-07-21 11:35:56 +0800

    数据库连接异常

---
+++ WlToolsLib/DBHelper/DbConnException.cs
@@ -0,0 +1,28 @@
+﻿// ------------------------------------
+// ProjectName: $safeprojectname$
+// FileName:    DbConnException
+// CreateTime:  2017/07/12 11:05:23
+// Creator:     weilai
+// FileRemark:  
+// ------------------------------------
+
+
+namespace WlToolsLib.DBHelper
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 数据连接异常类
+    /// </summary>
+    public class DbConnException : Exception
+    {
+        public DbConnException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
