9cf06bd88d5b9dc5760b1522fb0e465e8bb5977e
wlfsky <raddleoj@sina.com>
2017-07-21 11:31:27 +0800

    分页条件

---
+++ WlToolsLib/Pagination/PageCondition.cs
@@ -0,0 +1,24 @@
+﻿// ------------------------------------
+// ProjectName: $safeprojectname$
+// FileName:    PageCondition
+// CreateTime:  2017/07/13 17:55:46
+// Creator:     weilai
+// FileRemark:  
+// ------------------------------------
+
+
+namespace WlToolsLib.Pagination
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public class PageCondition
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+}
