349c50f323eb4f85d48b3076307be27c0906e85f
wlfsky <raddleoj@sina.com>
2017-08-09 00:17:57 +0800

    增加扩展hash： md5，sha1，sha256，keccak244

--- WlToolsLib/CryptoHelper/CryptoExpand.cs
+++ WlToolsLib/CryptoHelper/CryptoExpand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WlToolsLib.Expand;
 
 namespace WlToolsLib.CryptoHelper
 {
@@ -20,6 +21,69 @@ namespace WlToolsLib.CryptoHelper
             ICryptoHelper c = new AESCBCCrypto("AESCBC").SetData(key);
             return c.Decryption(self);
         }
+        #endregion
+
+        #region --Hash扩展--
+        /// <summary>
+        /// 转换md5
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToMd5(this string self)
+        {
+            if (self.NotNullEmpty())
+            {
+                ICryptoHelper ch = new MD5Hash();
+                return ch.Encryption(self);
+            }
+            return self;
+        }
+
+        /// <summary>
+        /// 转换SHA1
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToSHA1(this string self)
+        {
+            if (self.NotNullEmpty())
+            {
+                ICryptoHelper ch = new SHA1Hash();
+                return ch.Encryption(self);
+            }
+            return self;
+        }
+
+        /// <summary>
+        /// 转换SHA256
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToSHA256(this string self)
+        {
+            if (self.NotNullEmpty())
+            {
+                ICryptoHelper ch = new SHA256Hash();
+                return ch.Encryption(self);
+            }
+            return self;
+        }
+
+        /// <summary>
+        /// 转换Keccak224
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static string ToKeccak224(this string self)
+        {
+            if (self.NotNullEmpty())
+            {
+                ICryptoHelper ch = new Keccak224Hash();
+                return ch.Encryption(self);
+            }
+            return self;
+        }
+
         #endregion
     }
 }
