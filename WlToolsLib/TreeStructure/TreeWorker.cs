ec52d013a2289f3ffc9c6d0b8b695f46e86930c5
weilai <raddleoj@sina.com>
2017-08-23 09:35:19 +0800

    add find with path string and find with predicate

--- WlToolsLib/TreeStructure/TreeWorker.cs
+++ WlToolsLib/TreeStructure/TreeWorker.cs
@@ -337,5 +337,170 @@ namespace WlToolsLib.TreeStructure
         }
 
         #endregion
+        
+        #region --通过路径字符串查找--
+        /// <summary>
+        /// 定位路径字符串，/name/name 这种风格
+        /// 算法还需优化
+        /// </summary>
+        /// <param name="pathStr"></param>
+        /// <returns></returns>
+        public BaseLeaf<TKey> LocationPath(string pathStr)
+        {
+            var pathArray = pathStr.Split('/');
+            BaseLeaf<TKey> thisNode = this.TreeRoot;
+            if (pathArray.Any())
+            {
+                for (var i = 0; i < pathArray.Length; i++)
+                {
+                    if (i == 0 && pathArray[i].NullEmpty())
+                    {
+                        continue;
+                    }
+
+                    if (pathArray[i].NotNullEmpty() && thisNode.NotNull())
+                    {
+                        var nname = pathArray[i];
+                        thisNode = FindNodeByName(nname, thisNode);
+                        if (thisNode.IsNull())
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return thisNode;
+        }
+
+        protected BaseLeaf<TKey> FindNodeByName(string name, BaseLeaf<TKey> currNode)
+        {
+            if (currNode.IsNull()) { return default(TNode); }
+            if (currNode.Name.Equals(name)) { return currNode; }
+            if (currNode is TNode)
+            {
+                var n = currNode as TNode;
+                if (n.ChildrenNode.HasItem())
+                {
+                    foreach (var item in n.ChildrenNode)
+                    {
+                        if (item.Name.Equals(name))
+                        {
+                            return item as BaseLeaf<TKey>;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (currNode.Name.Equals(name))
+                {
+                    return currNode;
+                }
+            }
+            return default(TNode);
+        }
+
+        protected BaseLeaf<TKey> FindNodeByName(string name, List<TNode> currNode)
+        {
+            if (currNode.HasItem())
+            {
+                foreach (var item in currNode)
+                {
+                    if (item.Name.Equals(name))
+                    {
+                        return item;
+                    }
+                    if (item is TNode)
+                    {
+                        return FindNodeByName(name, item);
+                    }
+                }
+            }
+            return default(TNode);
+        }
+        #endregion
+
+        #region --指定查找谓词查找节点--
+        /// <summary>
+        /// 寻找下一层，递归层级
+        /// </summary>
+        /// <param name="currNode"></param>
+        /// <param name="findAction"></param>
+        /// <returns></returns>
+        public BaseLeaf<TKey> FindNode(TNode currNode, Func<BaseLeaf<TKey>, bool> predicate)
+        {
+            if (predicate.IsNull())
+            {
+                return default(TLeaf);
+            }
+            if (currNode.ChildrenNode.HasItem())
+            {
+                foreach (var item in currNode.ChildrenNode)
+                {
+                    if (item is TLeaf)
+                    {
+                        var l = item as TLeaf;
+                        if (predicate(l))
+                        {
+                            return l;
+                        }
+                    }
+                    if (item is TNode)
+                    {
+                        var n = item as TNode;
+                        var l = n as BaseLeaf<TKey>;
+                        if (predicate(l))
+                        {
+                            return l;
+                        }
+                        var findResult = FindNode(n, predicate);
+                        if (findResult.NotNull())
+                        {
+                            return findResult;
+                        }
+                    }
+                }
+            }
+            return default(TLeaf);
+        }
+
+        /// <summary>
+        /// 寻找当前子级层级，但是不递归
+        /// </summary>
+        /// <param name="currNode"></param>
+        /// <param name="findAction"></param>
+        /// <returns></returns>
+        public TLeaf FindCurrLevel(TNode currNode, Func<TLeaf, bool> predicate)
+        {
+            if (predicate.IsNull())
+            {
+                return default(TLeaf);
+            }
+            if (currNode.ChildrenNode.HasItem())
+            {
+                foreach (var item in currNode.ChildrenNode)
+                {
+                    if (item is TLeaf)
+                    {
+                        var l = item as TLeaf;
+                        if (predicate(l))
+                        {
+                            return l;
+                        }
+                    }
+                    if (item is TNode)
+                    {
+                        var n = item as TNode;
+                        if (predicate(n as TLeaf))
+                        {
+                            return n as TLeaf;
+                        }
+                    }
+
+                }
+            }
+            return default(TLeaf);
+        }
+        #endregion
     }
 }
